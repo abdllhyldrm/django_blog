d8347945d561ca2c3a5893f79b8f90ad7618c479
abdllhyldrm <abd.yldrm543@gmail.com>
2023-01-09 00:40:57 +0300

    kategori ve arama sayfası yapıldı

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from blog.views import iletisim, anasayfa
+from blog.views import iletisim, anasayfa, kategori, yazilarim
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
     path('iletisim', iletisim, name='iletisim'),
+    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
+    path('yazilarim', yazilarim, name='yazilarim')
 ]
--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from blog.models import YazilarModel
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def anasayfa(request):
-    context = {
-        'isim': 'Abdullah Yıldırım'
-    }
-    return render(request, 'pages/anasayfa.html', context={})
+    sorgu = request.GET.get('sorgu')
+    yazilar = YazilarModel.objects.order_by('-id')
+    if sorgu:
+        yazilar=yazilar.filter(
+            Q(baslik__icontains=sorgu) |
+            Q(icerik__incontains=sorgu)
+        )
+    sayfa = request.GET.get('sayfa')
+    paginator = Paginator(yazilar, 2)
+
+    return render(request, 'pages/anasayfa.html', context={
+        'yazilar': paginator.get_page(sayfa)
+    })
